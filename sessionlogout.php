801f7a0a599ee7c91cc2574b92a7561b0f055ba6
pheonix-12 <49608152+pheonix-12@users.noreply.github.com>
2019-09-04 15:03:18 +0530

    Delete sessionlogout.php

--- sessionlogout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-date_default_timezone_set("Asia/calcutta");
-echo"<h2><palign='right'>Session ended at", date("h:i:sa",time()),"</p></h2><br>";
-echo"<h2> Thank you",$_SESSION['user'],"</h2>";
-$sessiontime=time()-$_SESSION['start'];
-echo"<br><br> Your session duration: ",$sessiontime, "seconds", "</h2>";
-session_destroy();
-?>
