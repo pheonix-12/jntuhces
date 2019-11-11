c8f5d0db36bf1d7c29c8e96b7d64951883fd8023
pheonix-12 <49608152+pheonix-12@users.noreply.github.com>
2019-11-11 10:52:41 +0530

    Update 17.py

--- 17.py
+++ 17.py
@@ -2,13 +2,8 @@ f1=open("a.txt","r")
 l=[]
 for line in f1:
 	words=line.split()
-	for word in words:
-		for c in word:
-			l+=(list(c))
-u=list(set(l))
+u=list(set(words))
 x=[]
+u.sort()
 for i in u:
-	x.append(ord(i))
-x.sort()
-for i in x:
-	print chr(i)
+	print i;
